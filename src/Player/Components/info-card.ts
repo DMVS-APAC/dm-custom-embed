0547d926196eead3ea478a18a587c398a4f7357b
Yudhi Satrio <isatrio.is.me@gmail.com>
2021-02-11 12:24:10 +0700

    Infocard improvement
    1. Separated infocard style to it's own file
    2. Added collapse button
    3. Fixed shrinking image logo when the description collapsed

--- src/Player/Components/info-card.ts
+++ src/Player/Components/info-card.ts
@@ -21,8 +21,27 @@ export default class InfoCard {
             '<a href="$1">$1</a>'
         ) : data.description;
         videoDesc.innerHTML = desc;
-        videoDesc.className = 'dm__video-desc' + (fullLength ? ' dm__full-desc' : '');
+        videoDesc.className = 'dm__video-desc';
 
+        const collapseButton = document.createElement('button');
+        collapseButton.innerHTML = `<svg width="9" height="10" viewBox="0 0 9 10" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+<rect width="9" height="10" fill="url(#pattern0)"/>
+<defs>
+<pattern id="pattern0" patternContentUnits="objectBoundingBox" width="1" height="1">
+<use xlink:href="#image0" transform="scale(0.111111 0.1)"/>
+</pattern>
+<image id="image0" width="9" height="10" xlink:href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAkAAAAKCAYAAABmBXS+AAAAXElEQVQoFYXQwQmAMAyF4Q+vTuAQzuEgDqKDOIhzOIQTeBWlh0IotA2EJLwf8nhwYtapDy8OTDU2QbkfbBhLOANx3lgxZDiK5X5hSWApxLsJNd8l43vNeDeCbpg/LfMs1IxQsGEAAAAASUVORK5CYII="/>
+</defs>
+</svg>
+`;
+        collapseButton.className = 'dm__collapse-button';
+        collapseButton.setAttribute('aria-label', 'Collapse video description');
+
+        collapseButton.addEventListener('click', e => {
+            this.collapseDesc(videoDesc, collapseButton);
+        });
+
+        videoTitle.insertAdjacentElement('afterbegin', collapseButton);
         textWrapper.append(videoTitle);
         textWrapper.append(videoDesc);
 
@@ -46,6 +65,16 @@ export default class InfoCard {
         if (this.infoCardEl) {
             this.infoCardEl.remove();
             delete this.infoCardEl;
+        }
+    }
+
+    private collapseDesc(descEl: HTMLElement, buttonEl: HTMLButtonElement): void {
+        if (descEl.classList.contains('dm__full-desc')) {
+            descEl.classList.remove('dm__full-desc');
+            buttonEl.classList.remove('collapsed');
+        } else {
+            descEl.classList.add('dm__full-desc');
+            buttonEl.classList.add('collapsed');
         }
     }
 }
