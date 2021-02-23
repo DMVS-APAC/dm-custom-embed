28fc15e3f2fea9dd02d0a8c6fea1215dca12b826
Yudhi Satrio <isatrio.is.me@gmail.com>
2021-02-23 10:39:56 +0700

    Change the arrow color darker

--- dist/dm-amp.js
+++ dist/dm-amp.js
@@ -2879,7 +2879,7 @@ var InfoCard = /** @class */ (function () {
         videoDesc.innerHTML = desc;
         videoDesc.className = 'dm__video-desc';
         var collapseButton = document.createElement('button');
-        collapseButton.innerHTML = "<svg width=\"20\" height=\"12\" viewBox=\"0 0 20 12\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\n<line x1=\"0.707107\" y1=\"1.29289\" x2=\"10.253\" y2=\"10.8388\" stroke=\"#BBBCBC\" stroke-width=\"2\"/>\n<line x1=\"9.1924\" y1=\"11.1924\" x2=\"19.0919\" y2=\"1.29289\" stroke=\"#BBBCBC\" stroke-width=\"2\"/>\n</svg>";
+        collapseButton.innerHTML = "<svg width=\"20\" height=\"12\" viewBox=\"0 0 20 12\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\n<line x1=\"0.707107\" y1=\"1.29289\" x2=\"10.253\" y2=\"10.8388\" stroke=\"#AAAAAA\" stroke-width=\"2\"/>\n<line x1=\"9.1924\" y1=\"11.1924\" x2=\"19.0919\" y2=\"1.29289\" stroke=\"#AAAAAA\" stroke-width=\"2\"/>\n</svg>";
         collapseButton.className = 'dm__collapse-button';
         collapseButton.setAttribute('aria-label', 'Collapse video description');
         collapseButton.addEventListener('click', function (e) {
--- src/Player/Components/info-card.ts
+++ src/Player/Components/info-card.ts
@@ -25,8 +25,8 @@ export default class InfoCard {
 
         const collapseButton = document.createElement('button');
         collapseButton.innerHTML = `<svg width="20" height="12" viewBox="0 0 20 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-<line x1="0.707107" y1="1.29289" x2="10.253" y2="10.8388" stroke="#BBBCBC" stroke-width="2"/>
-<line x1="9.1924" y1="11.1924" x2="19.0919" y2="1.29289" stroke="#BBBCBC" stroke-width="2"/>
+<line x1="0.707107" y1="1.29289" x2="10.253" y2="10.8388" stroke="#AAAAAA" stroke-width="2"/>
+<line x1="9.1924" y1="11.1924" x2="19.0919" y2="1.29289" stroke="#AAAAAA" stroke-width="2"/>
 </svg>`;
         collapseButton.className = 'dm__collapse-button';
         collapseButton.setAttribute('aria-label', 'Collapse video description');
